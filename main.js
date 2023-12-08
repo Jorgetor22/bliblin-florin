f02009019b713c7ad75a915529bdb62041181788
Jorgetor22 <153325202+Jorgetor22@users.noreply.github.com>
2023-12-08 17:23:24 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+onload = () =>{
+        document.body.classList.remove("container");
+        
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    var audio = new Audio('img/Floricienta.mp3'); // Reemplaza 'audio.mp3' con la ruta de tu archivo de audio
+    audio.play();
+});
